46a35e39eb13cb66e580bb2696c45b2c39d5d54b
Mikko Forsström <pekkisx@gmail.com>
2023-12-10 00:00:48 +0200

    Next nuxt

--- src/services/hatch/next/nextHatch.tsx
+++ src/services/hatch/next/nextHatch.tsx
@@ -3,7 +3,7 @@ import { HatchDataWithoutDay } from "@/services/hatch";
 import next from "@/assets/apple-next.jpeg";
 
 const nextHatch: HatchDataWithoutDay = {
-  title: "Seuraava luukku",
+  title: "NeXT ❤️ Apple",
   image: {
     src: next,
     alt: ""
@@ -11,10 +11,15 @@ const nextHatch: HatchDataWithoutDay = {
   content: [
     {
       type: "markdown",
-      text: `Joulukuun 20. päivänä vuonna 1996 Apple Computer ilmoitti aikovansa ostaa NeXTin.
+      text: `Joulukuun 20. päivänä vuonna 1996 [Apple Computer](https://en.wikipedia.org/wiki/Apple_Inc.) ilmoitti aikovansa ostaa [NeXTin](https://en.wikipedia.org/wiki/NeXT).
 
+NeXT oli firma, jonka [Steve Jobs](https://en.wikipedia.org/wiki/Steve_Jobs) oli perustanut vuonna 1985, kun hänet oli potkittu ulos Applen johdosta. NeXTin työasemat olivat törkykalliita, mutta hienoja, ja masiinoiden käyttöjärjestelmä, [NeXTSTEP](https://en.wikipedia.org/wiki/NeXTSTEP), oli vuosia aikaansa edellä.
 
-      On December 20, 1996, Apple Computer announced its intention to acquire NeXT`
+Muistan, kuinka haaveilin lapsena NeXTistä lukiessani siitä Mikrobitin sivuilta. [Sir Tim Berners-Leen](https://en.wikipedia.org/wiki/Tim_Berners-Lee) ei tarvinnut haaveilla, koska Cernissä näitä ihmevehkeitä oli. Miekkonen kehittikin NeXT-koneella [World Wide Webin](https://en.wikipedia.org/wiki/World_Wide_Web) ensimmäisen version. Myös Doom ja Quake kehitettiin NeXTillä.
+
+Jobsista tuli yrityskaupan myötä Applen neuvonantaja, mutta pian hän oli jo väliaikainen toimitusjohtaja, ja antanut potkut kaikille mielestään idiooteille - etenkin jokaiselle, joka oli ollut antamassa hänelle itselleen potkuja.
+
+Loppu onkin historiaa. Stevestä tuli pomo, NextSTEPistä tuli [Mac OS X](https://en.wikipedia.org/wiki/MacOS), ja Applesta tuli vähän muutakin kuin tietokonefirma.`
     }
   ]
 };
