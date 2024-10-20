49b4c3420af9b2088323101dbf2cc6f81be08d73
Mikko Forsström <pekkisx@gmail.com>
2024-10-20 16:30:12 +0300

    Heps kukkuu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ type Props = {
   children: ReactNode;
 };
 
-export const metadatta: Metadata = {
+export const metadata: Metadata = {
   title: "Pekkiksen koodijoulukalenteri",
   openGraph: {
     type: "website",
