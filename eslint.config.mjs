f46d5908418280190cc51e9862e598fd9fc46639
Mikko Forsström <pekkisx@gmail.com>
2024-10-20 14:05:25 +0300

    Wut the fugg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,8 @@ import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import tseslint from "typescript-eslint";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
